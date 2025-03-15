cf582fe69adeb43df418be47fd7e0903c3d4bf96
MuhammadMaaz7 <maazawan100@gmail.com>
2025-03-15 14:13:49 +0500

    Population Initialized

--- GA.py
+++ GA.py
@@ -1,5 +1,6 @@
 import re
 import random
+import calendar
 
 def is_valid_date(date_str):
     
@@ -35,28 +36,141 @@ def is_valid_date(date_str):
     
     return True
 
-def initialize_population(pop_size):
+def initialize_population(categories,totalTC, TCPerCat):
     population = []
-    count = 0
+    catTCCount = 0
     
-    while count < pop_size:
-        rand_day = str(random.randint(1,31)).zfill(2)
-        rand_month = str(random.randint(1,12)).zfill(2)
-        rand_year = str(random.randint(0000,9999)).zfill(4)
-        
-        date = rand_day + '/' + rand_month + '/' + rand_year 
-        print(date)
-        
-        if is_valid_date(date):
-            print("Valid date: ",date)
-            date_tuple = (rand_day,rand_month,rand_year)
-            population.append(date_tuple) 
+    for category in categories:
+        count = 0
+        while count < TCPerCat:
+            population.append(generate_test_case(category))
             count += 1
-        else:
-            print("Invalid date: ",date)
-            continue
-        
-    print(population)
-        
+            catTCCount += 1
+    
+    totalTC -= catTCCount 
+    while totalTC >0:
+        population.append(generate_random())
+        totalTC -= 1
+    
+    return population  
 
-initialize_population(100)
+def generate_test_case(category):
+    if category == "Leap Year":
+        return generateLeapYear()
+    elif category == "30-day month":
+        return generate30DayMonth()
+    elif category == "31-day month":
+        return generate31DayMonth()
+    elif category == "Day > 31":
+        return generateDayGreater31()
+    elif category == "Month > 12":
+        return generateMonthGreater12()
+    elif category == "Non-leap February 29":
+        return generateNonLeapFebruary29()
+    elif category == "Min/Max Year":
+        return generateMinMaxYear()
+    elif category == "day/month transitions":
+        return generateDayMonthTransitions()
+    
+def generate_random():
+    day = str(random.randint(1,31)).zfill(2)
+    month = str(random.randint(1,12)).zfill(2)
+    year = str(random.randint(0,9999)).zfill(4)
+    
+    return f"{day}/{month}/{year}"  
+
+def generateLeapYear():
+    day = str(random.randint(1,29)).zfill(2)
+    month = str(random.randint(1,12)).zfill(2)
+    year = random.randint(0,9999)
+    
+    while 1:
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0) :
+            break
+        else:
+            year = random.randint(0,9999)
+    
+    year = str(year).zfill(4)
+    
+    return f"{day}/{month}/{year}"
+        
+def generate30DayMonth():
+    day = str(random.randint(1,30)).zfill(2)
+    year = str(random.randint(0,9999)).zfill(4)
+    month = str(random.choice([11,9,6,4])).zfill(2)
+    
+    return f"{day}/{month}/{year}"
+     
+def generate31DayMonth():
+    day = str(random.randint(1,31)).zfill(2)
+    year = str(random.randint(0,9999)).zfill(4)
+    month = str(random.choice([12,10,8,7,5,3,1])).zfill(2)
+    
+    return f"{day}/{month}/{year}"
+
+def generateDayGreater31():
+    day = str(random.randint(31,99)).zfill(2)
+    year = str(random.randint(0,9999)).zfill(4)
+    month = str(random.randint(1,12)).zfill(2)
+    
+    return f"{day}/{month}/{year}"
+    
+def generateMonthGreater12():
+    day = str(random.randint(1,31)).zfill(2)
+    year = str(random.randint(0,9999)).zfill(4)
+    month = str(random.randint(13,99)).zfill(2)
+    
+    return f"{day}/{month}/{year}"
+
+def generateNonLeapFebruary29():
+    day = "29"
+    month = "02"
+    year = random.randint(0,9999)
+    
+    while 1:
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0) :
+            year = random.randint(0,9999)
+        else:
+            break
+    
+    year = str(year).zfill(4)
+    
+    return f"{day}/{month}/{year}"
+
+def generateMinMaxYear():
+    day = str(random.randint(1,28)).zfill(2)
+    year = str(random.choice([0000,9999])).zfill(4)
+    month = str(random.randint(1,12)).zfill(2)
+    
+    return f"{day}/{month}/{year}"
+
+def generateDayMonthTransitions():
+    year = random.randint(0,9999)
+    month = random.randint(1,12)
+    day = calendar.monthrange(year,month)[1]
+    day = str(day).zfill(2)
+    month = str(month).zfill(2)
+    year = str(year).zfill(4)
+    
+    return f"{day}/{month}/{year}"
+            
+def main():
+    categories = ["Leap Year","30-day month","31-day month", "Day > 31", "Month > 12", "Non-leap February 29", "Min/Max Year", "day/month transitions"]
+    selectedCategories = []
+    totalTestCases = 0
+    
+    print("Please Select the Target Categories (1/0)") 
+    for  category in categories:
+        choice = input(f"{category}: ")
+        
+        if choice == "1":
+            selectedCategories.append(category)
+        else:
+            continue
+    
+    testCasesPerCategory = 1
+    totalTestCases = 25
+    
+    initialize_population(selectedCategories,totalTestCases,testCasesPerCategory)
+    
+main()
